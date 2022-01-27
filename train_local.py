75c1f2718237e038844318b799341ca5ea92d5c7
sandracl72 <sandra.carrascol@uah.es>
2022-01-27 08:27:46 +0100

    option train with whole isic by patient id

--- train_local.py
+++ train_local.py
@@ -18,12 +18,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if __name__ == "__main__":
     parser = ArgumentParser() 
     parser.add_argument("--model", type=str, default='efficientnet-b2') 
-    parser.add_argument("--tags", type=str, default='local_training') 
+    parser.add_argument("--tags", type=str, default='local_training', help="Use 'whole' for training with whole dataset") 
     parser.add_argument("--log_interval", type=int, default='100')  
     parser.add_argument("--epochs", type=int, default='15')  
     parser.add_argument("--early_stopping", type=int, default='3')  
     parser.add_argument("--num_partitions", type=int, default='10') 
-    parser.add_argument("--partition", type=int, default='0', help="To train with the whole dataset use 'whole' ")  
+    parser.add_argument("--partition", type=int, default='0')  
     args = parser.parse_args()
     
     wandb.init(project="dai-healthcare" , entity='eyeforai', group='local_training', tags=[args.tags], config={"model": args.model})
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     # trainset, testset, num_examples = utils.load_partition(trainset, testset, num_examples, idx=args.partition, num_partitions=args.num_partitions)
     # trainset, testset, num_examples = utils.load_experiment_partition(trainset, testset, num_examples, idx=args.partition)
     
-    if args.partition == 'whole':
+    if 'whole' in args.tags:
         trainset, testset, num_examples = utils.load_isic_by_patient_server()
     else:
         trainset, testset, num_examples = utils.load_isic_by_patient_client(args.partition) 
